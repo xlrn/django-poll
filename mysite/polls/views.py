ed39ea92bd851184c2e32b5205d1bd8d647455c2
BuildTools <ncl.yim@gmail.com>
2019-02-05 14:27:34 -0800

    progress on details view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,8 +12,9 @@ def detail(request, question_id):
 	return HttpResponse("You're looking ay question %s." % question_id)
 
 def results(request, question_id):
-	response = "You're looking at the results of qurstion %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)

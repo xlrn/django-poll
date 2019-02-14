e34dce35390f7f0b6e25f22e37b3c71b301244c1
BuildTools <ncl.yim@gmail.com>
2019-02-14 13:52:45 -0800

    added more unit tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -34,3 +35,30 @@ class QuestionModelTests(TestCase):
 		time = timezone.now() - datetime.timedelta(days=1, seconds=1)
 		old_question = Question(pub_date=time)
 		self.assertIs(old_question.was_published_recently(), False)
+
+def create_question(question_text, days):
+	"""
+	Create a question with the given `question_text` and published the
+	given number of `days` offset to now (negative for questions published
+	in the past, positive for questions that have yet to be published).
+	"""
+	time = timezone.now() + datetime.timedelta(days=days)
+	return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+	def test_no_questions(self):
+		"""
+		If no questions exist, an appropriate message is displayed.
+		"""
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_past_question(self):
+		"""
+		Questions with a pub_date in the past are displayed in the index page.
+		"""
+		create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(response.context['latest_question_list'],['<Question: Past question.>'])
